4e35933cd73b5b1f3d36ef4c41d9498f81e7ad88
Darko Stevanovic <thefirstpresenter@gmail.com>
2019-07-31 22:41:27 +0200

    feat(public-pages): add form to public pages

--- frontend/src/components/pages/Login/Login.js
+++ frontend/src/components/pages/Login/Login.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../public-styles/PublicStyles.scss'
 import Card from '../../ui/Card/Card'
 import LabeledInput from '../../ui/LabeledInput/LabeledInput'
@@ -6,32 +6,60 @@ import Button from '../../ui/Button/Button'
 import {Link} from 'react-router-dom'
 
 function LoginPage(props) {
+
+    const [email, setEmail] = useState('johndoe@gmail.com')
+    const [password, setPassword] = useState('john123')
+
+    function handleSubmit(event) {
+        event.preventDefault()
+
+        console.log('email: ', email)
+        console.log('pass: ', password)
+    }
+
     return (
         <div className="public-page">
             <Card>
-                <h1>Login</h1>
+
+                <h1>Intensity Login</h1>
+
                 <div className="funny-text">
                     <p>Feeling strong today?</p>
                 </div>
-                <LabeledInput
-                    label="Email"
-                    type="email"
-                />
-                <LabeledInput
-                    label="Password"
-                    type="password"
-                />
-                <Button>
-                    Submit
-                </Button>
+
+                <form onSubmit={handleSubmit}>
+
+                    <LabeledInput
+                        label="Email"
+                        value={email}
+                        type="email"
+                        handleInput={setEmail}
+                    />
+
+                    <LabeledInput
+                        label="Password"
+                        value={password}
+                        type="password"
+                        handleInput={setPassword}
+                    />
+
+                    <Button
+                        type="submit"
+                    >
+                        Submit
+                    </Button>
+
+                </form>
+
                 <p>
                     Don't have an account?
                     Feel free to
                     <Link to="/register/"> register</Link>.
                 </p>
+
             </Card>
         </div>
-)
+    )
 }
 
 export default LoginPage
--- frontend/src/components/pages/Register/Register.js
+++ frontend/src/components/pages/Register/Register.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../public-styles/PublicStyles.scss'
 import Card from '../../ui/Card/Card'
 import LabeledInput from '../../ui/LabeledInput/LabeledInput'
@@ -6,37 +6,72 @@ import Button from '../../ui/Button/Button'
 import {Link} from 'react-router-dom'
 
 function RegisterPage(props) {
+
+    const [name, setName] = useState('John')
+    const [lastname, setLastname] = useState('Doe')
+    const [email, setEmail] = useState('johndoe@gmail.com')
+    const [password, setPassword] = useState('john123')
+
+    function handleSubmit(event) {
+        event.preventDefault()
+
+        console.log('name: ', name)
+        console.log('lastname: ', lastname)
+        console.log('email: ', email)
+        console.log('pass: ', password)
+    }
+
     return (
         <div className="public-page">
             <Card>
-                <h1>Register</h1>
+
+                <h1>Intensity Register</h1>
+
                 <div className="funny-text">
                     <p>Welcome to Intensity app! </p>
                     <p> Feeling ready to enter the chamber of strength?</p>
                 </div>
-                <LabeledInput
-                    label="NAME"
-                    type="text"
-                />
-                <LabeledInput
-                    label="Lastname"
-                    type="text"
-                />
-                <LabeledInput
-                    label="Email"
-                    type="email"
-                />
-                <LabeledInput
-                    label="Password"
-                    type="password"
-                />
-                <Button>
-                    Submit
-                </Button>
+
+                <form onSubmit={handleSubmit}>
+
+                    <LabeledInput
+                        label="Name"
+                        value={name}
+                        type="text"
+                        handleInput={setName}
+                    />
+
+                    <LabeledInput
+                        label="Lastname"
+                        value={lastname}
+                        type="text"
+                        handleInput={setLastname}
+                    />
+
+                    <LabeledInput
+                        label="Email"
+                        value={email}
+                        type="email"
+                        handleInput={setEmail}
+                    />
+
+                    <LabeledInput
+                        label="Password"
+                        value={password}
+                        type="password"
+                        handleInput={setPassword}
+                    />
+
+                    <Button>
+                        Submit
+                    </Button>
+
+                </form>
                 <p>
                     Already have an account? Just
                     <Link to="/login/"> login</Link>.
                 </p>
+
             </Card>
         </div>
     )
