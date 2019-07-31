07f9fae75afcb37d040866001ec6e8b6e0ce40d7
Darko Stevanovic <thefirstpresenter@gmail.com>
2019-07-31 19:27:06 +0200

    feat(router): add root router component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import LoginPage from './components/pages/Login/Login.js'
-import App from './App.js'
-import * as serviceWorker from './serviceWorker';
+import { createBrowserHistory } from 'history'
+import {Router} from 'react-router-dom'
+import Routes from './routes/routes'
+import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<LoginPage/>, document.getElementById('root'));
+const history = createBrowserHistory()
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+function IntensityApp() {
+    return (
+        <Router history={history}>
+            <Routes/>
+        </Router>
+    )
+}
+
+const root =  document.getElementById('root')
+
+ReactDOM.render(<IntensityApp/>, root);
+
+
 serviceWorker.unregister();
