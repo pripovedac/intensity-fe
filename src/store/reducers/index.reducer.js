ece8c349cb5cd435302ce3dc126d65b71b0d63bc
Darko Stevanovic <thefirstpresenter@gmail.com>
2019-08-10 10:02:47 +0200

    fix(connected-router): cleanup

--- src/store/reducers/index.reducer.js
+++ src/store/reducers/index.reducer.js
@@ -1,11 +1,9 @@
 import {combineReducers} from 'redux'
-import {connectRouter} from 'connected-react-router'
 import authReducer from './auth.reducer'
 import globalReducer from './global.reducer'
 
 function rootReducer(history) {
     return combineReducers({
-        router: connectRouter(history),
         auth: authReducer,
         global: globalReducer
     })
