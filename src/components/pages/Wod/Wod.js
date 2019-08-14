eb0bad4f6fb167023fb59bf8b99090a907ed16fb
Darko Stevanovic <thefirstpresenter@gmail.com>
2019-08-14 17:46:23 +0200

    feat(complete-wod): create user wod view

--- src/components/pages/Wod/Wod.js
+++ src/components/pages/Wod/Wod.js
@@ -3,24 +3,40 @@ import TrainingForm from '../../ui/TrainingForm/TrainingForm'
 import Navigation from '../../ui/Navigation/Navigation.js'
 import ExerciseForm from '../../ui/ExerciseForm/ExerciseForm'
 import ExerciseList from '../../ui/ExerciseList/ExerciseList'
+import CompleteWod from '../../ui/CompleteWod/CompleteWod'
 import './Wod.scss'
 import {bindActionCreators} from 'redux'
 import {setWodMode} from '../../../store/actions/global.action'
+import {submitWod} from '../../../store/actions/wod.action'
 import {connect} from 'react-redux'
-import {selectMode} from "../../../store/selectors/global.selector";
+import {selectMode} from '../../../store/selectors/global.selector'
+import {selectWodWithExercises} from '../../../store/selectors/wod.selector'
 
 function WodPage(props) {
     function displayContent() {
-        if (props.mode === 'wod') {
+        if (props.mode === 'regular') {
+            return <CompleteWod />
+        } else if (props.mode === 'wod') {
             return <TrainingForm/>
         } else if (props.mode === 'exercise') {
             return (
-                <div className="exercise-container">
-                    <ExerciseForm/>
-                    <ExerciseList/>
+                <div className="exercise-page">
+                    <div className="exercise-container">
+                        <ExerciseForm/>
+                        <ExerciseList exercises={props.wodWithExercises.exercises}/>
+                    </div>
+                    <button
+                        disabled={!props.wodWithExercises.exercises.length}
+                        onClick={submitWod}>
+                        Submit complete WOD
+                    </button>
                 </div>
             )
         }
+    }
+
+    function submitWod() {
+        props.submitWod(props.wodWithExercises)
     }
 
     return (
@@ -28,9 +44,11 @@ function WodPage(props) {
             <Navigation/>
             <div className="wod-container">
                 <div className="hulk-container">
-                    <img src="./images/hulk.jpg" alt="Hulk" width={300}/>
+                    <img src="./images/hulk.jpg"
+                         alt="Hulk"
+                         width={300}/>
                 </div>
-                    {displayContent()}
+                {displayContent()}
             </div>
         </div>
     )
@@ -38,12 +56,17 @@ function WodPage(props) {
 
 function mapStateToProps(state) {
     return {
-        mode: selectMode(state)
+        mode: selectMode(state),
+        wodWithExercises: selectWodWithExercises(state),
     }
 }
 
 function mapDispatchToProps(dispatch) {
-    return bindActionCreators({setWodMode}, dispatch)
+    return bindActionCreators(
+        {
+            setWodMode,
+            submitWod
+        }, dispatch)
 }
 
 export default connect(
---
+++ src/components/ui/CompleteWod/CompleteWod.js
@@ -0,0 +1,94 @@
+import React from 'react'
+import './CompleteWod.scss'
+import {selectMode} from "../../../store/selectors/global.selector";
+import {selectWodWithExercises} from "../../../store/selectors/wod.selector";
+import {bindActionCreators} from "redux";
+import {setWodMode} from "../../../store/actions/global.action";
+import {submitWod} from "../../../store/actions/wod.action";
+import {connect} from "react-redux";
+import {selectUser} from "../../../store/selectors/auth.selector";
+import ExerciseList from "../ExerciseList/ExerciseList";
+import {FaPencilAlt} from 'react-icons/fa'
+import {IoMdCheckmarkCircle, IoMdCloseCircle} from 'react-icons/io'
+
+function CompleteWod(props) {
+    function displayEditButton() {
+        if (props.user.role != 'user') {
+            return (
+                <button onClick={props.setWodMode}>
+                    <FaPencilAlt/>
+                </button>
+            )
+        }
+    }
+
+    function displayWodInfo() {
+        const wordRound = props.wod.roundNumber > 1 ?
+            'rounds' :
+            'round'
+
+        const wodName = props.wod.name
+        const title = wodName != 'crossfit' && wodName != 'lightfit' ?
+            <div className="title-container">
+                <h1>{`${wodName}`}</h1>
+                <h2>{`${props.wod.trainingType} ${props.wod.globalType} workout of the Day`}</h2>
+            </div> :
+            <h1> {`${props.wod.trainingType} ${props.wod.globalType} workout of the Day`}</h1>
+
+        return (
+            <div className="wod-info">
+                {title}
+                {/*todo: fix date and add time} */}
+                <p>{`${props.wod.date}`}</p>
+                <p>{`${props.wod.roundNumber} ${wordRound} for ${props.wod.duration}
+                 minutes with ${props.wod.trainer}`}</p>
+            </div>
+        )
+    }
+
+    function displayExerciseList() {
+        return (
+            <ExerciseList
+                exercises={props.wod.exercises}/>
+        )
+    }
+
+    function displaySubmitButton() {
+        return (
+            <button className="submit-button">
+                <IoMdCheckmarkCircle/>
+            </button>
+        )
+    }
+
+    return (
+        <div className="complete-wod">
+            {displayEditButton()}
+            {displayWodInfo()}
+            {displayExerciseList()}
+            {displaySubmitButton()}
+        </div>
+    )
+}
+
+function mapStateToProps(state) {
+    return {
+        mode: selectMode(state),
+        wod: selectWodWithExercises(state),
+        user: selectUser(state)
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators(
+        {
+            setWodMode,
+            submitWod,
+        }, dispatch)
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(CompleteWod)
+
