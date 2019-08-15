de4a8651fc4140d7889b6414addb832343d9df25
Darko Stevanovic <thefirstpresenter@gmail.com>
2019-08-15 16:28:08 +0200

    feat(wod): update redux wod object

--- src/store/reducers/wod.reducer.js
+++ src/store/reducers/wod.reducer.js
@@ -1,11 +1,17 @@
 import * as actions from '../actions/wod.action'
 
-const initialState = {}
+const initialState = {
+    active: {},
+    new: {}
+}
 
 export default function (state = initialState, action) {
     switch (action.type) {
         case actions.WOD_ADD: {
-            return action.payload
+            return {
+                ...state,
+                new: action.payload
+            }
         }
 
         case actions.WOD_SUBMIT_ABORT: {
