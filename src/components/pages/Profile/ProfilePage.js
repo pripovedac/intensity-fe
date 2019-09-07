aa35308dd9bae7bb7472a72b8434ff3fe6aef679
Darko Stevanovic <thefirstpresenter@gmail.com>
2019-09-07 17:52:59 +0200

    feat(profile-page): add profile-page component

--- src/components/pages/Profile/ProfilePage.js
+++ src/components/pages/Profile/ProfilePage.js
@@ -1,11 +1,51 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {useSelectorWrapper} from '../../custom-hooks/useReduxHooks'
 import ProfilePicture from '../../ui/ProfilePicture/ProfilePicture'
+import UserForm from '../../ui/UserForm/UserForm'
+import {selectUser} from '../../../store/selectors/auth.selector'
+import {toUserDateFormat} from '../../../services/dates'
+import classNames from 'classnames'
 import './ProfilePage.scss'
 
-export default function ProfilePage(props)  {
-    return(
-        <div className="profile-page">
-            <ProfilePicture/>
+export default function ProfilePage() {
+    const user = useSelectorWrapper(selectUser)
+    const [loading, setLoading] = useState(false)
+
+    function displayUneditableInfo(user) {
+        if (user.isActive) {
+            return (
+                <div className="uneditable-info">
+                    <p>{`Remained trainings: ${user.trainingNum}`}</p>
+                    <p>{`Activation date: ${toUserDateFormat(user.activationDate)}`}</p>
+                </div>
+            )
+        } else {
+            return (
+                <div className="uneditable-info">
+                    <p>Account is not activated</p>
+                </div>
+            )
+        }
+    }
+
+    const classes = classNames({
+        'profile-page': true,
+        'loading-profile': loading
+    })
+
+    return (
+        <div className={classes}>
+
+            <h1>{`${user.name} ${user.lastname}`}</h1>
+            <h2>{user.role == 'user' ? 'Member' : `${user.role}`}</h2>
+
+            <div className="editable-info">
+                <UserForm setLoading={setLoading}/>
+                <ProfilePicture/>
+            </div>
+
+            {displayUneditableInfo(user)}
+
         </div>
     )
 }
